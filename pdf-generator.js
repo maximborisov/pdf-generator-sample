2c2cce8aef1efd1b4fcf2eee31efacfac20f93bc
Борисов Максим Львович <Maxim.Borisov@russianpost.ru>
2021-04-14 16:02:11 +0300

    Fixed recepient layout

--- pdf-generator.js
+++ pdf-generator.js
@@ -7,11 +7,16 @@ async function createPDF(data){
 
 	//var templateHtml = fs.readFileSync(path.join(process.cwd(), 'template.html'), 'utf8');
 	var templateHtml = fs.readFileSync(path.join(process.cwd(), 'f119.html'), 'utf8');
+	// handlebars.registerHelper("if", function(conditional, options) {
+	// 	if (conditional) {
+	// 	  return options.fn(this);
+	// 	}
+	//   });
 	var template = handlebars.compile(templateHtml);
-	//var html = template(data);
-	var html = template({});
+	var html = template(data);
+	//var html = template({});
 
-	var milis = new Date();
+	var milis = new Date();handlebars
 	milis = milis.getTime();
 
 	var pdfPath = path.join('pdf', `${data.name}-${milis}.pdf`);
@@ -47,14 +52,34 @@ async function createPDF(data){
 	await browser.close();
 }
 
+// const data = {
+// 	title: "A new Brazilian School",
+// 	date: "05/12/2018",
+// 	name: "Rodolfo Luis Marcos",
+// 	age: 28,
+// 	birthdate: "12/07/1990",
+// 	course: "Computer Science",
+// 	obs: "Graduated in 2014 by Federal University of Lavras, work with Full-Stack development and E-commerce."
+// }
+
 const data = {
-	title: "A new Brazilian School",
-	date: "05/12/2018",
-	name: "Rodolfo Luis Marcos",
-	age: 28,
-	birthdate: "12/07/1990",
-	course: "Computer Science",
-	obs: "Graduated in 2014 by Federal University of Lavras, work with Full-Stack development and E-commerce."
-}
+	barcode: '14089207000016',
+	sender_name: 'Иванов Иван Иванович',
+	sender_index: '115516',
+	sender_address: '350010, Краснодарский край, г Краснодар, ул Длинная (Садовое товарищество Любитель), д 32',
+	sender_note: 'Вариант № 2 (99х144  - отступ сверху 1мм)',
+	simple_notice: true,
+	registered_notice: true,
+	letter: true,
+	wrapper: true,
+	parcel: true,
+	first_class_departure: true,
+	registered: true,
+	declared_value: true,
+	ordinary: true,
+	recipient_name: 'Мищенко Василий Иванович',
+	recipient_index: '423570',
+	recipient_address: 'НИЖНЕКАМСК ТАТАРСТАН РЕСПУБЛИКА 423570',
+};
 
 createPDF(data);
